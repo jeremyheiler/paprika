0e6810fe7125297052d68de89953fea6a6cc2bfb
Jeremy Heiler <jeremyheiler@gmail.com>
2013-05-10 17:38:17 -0400

    Add dependency: clj-http

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/jeremyheiler/paprika"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-http "0.7.2"]])
